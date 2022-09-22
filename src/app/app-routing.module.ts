f3fde6eab353a779ef68bace12d8d056629a4357
Chidambaram Sathappan <ganeshvignesh9904@gmail.com>
2022-09-22 19:53:56 +0530

    Connected Components through Routing
    Signed-off-by: Chidambaram Sathappan <ganeshvignesh9904@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentOneComponent } from './components/component-one/component-one.component';
+import { ComponentTwoComponent } from './components/component-two/component-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "one", component: ComponentOneComponent}, 
+  {path: "two", component: ComponentTwoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
